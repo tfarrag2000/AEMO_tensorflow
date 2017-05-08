fbf9eef84263c3fe35352c5e139f538090bb49dc
tfarrag2000 <tfarrag2000@hotmail.com>
2017-05-08 08:18:07 +0300

    commited

--- AEMO_tensorflow/AEMO_tensorflow.py
+++ AEMO_tensorflow/AEMO_tensorflow.py
@@ -11,11 +11,12 @@ import itertools
 
 start_time = time.time()
 
-df= pd.DataFrame.from_csv(r'C:\Users\T\OneDrive\My Research\Tamer_Ehab_STLF\Programs For Local Machine\AEMO1_data.csv')
 
+xls_file = pd.ExcelFile(r'C:\Users\T\OneDrive\My Research\Tamer_Ehab_STLF\Programs For Local Machine\AEMO1_data.xlsx')
+df = xls_file.parse('AEMO1_data')
 #~~~~~~~~~~~~~~ Normalization from (-1 to 1) ~~~~~~~~~~~~
 #OldData=df.loc[df['month'].isin([1,2,3,4,5,6,7,8,9,10,11,12])].values[:,7];
-OldData=df
+OldData=df['load'].values
 MaxLoad  =np.amax(OldData,axis=0)
 MinLoad  =np.amin(OldData,axis=0)   
 OldData[:] = (2 *  (OldData[:] -MinLoad) /(MaxLoad-MinLoad))  -1
@@ -42,18 +43,20 @@ for i in range(T-1):
 columnsNames= columnsNames=  ['id'] + [ 'x'+str(i) for i in range(1, dim+1)] + ['Y']
 newDataFrame=pd.DataFrame(newData,columns =columnsNames);
 
-Train = newDataFrame[0:T-31-2].drop('id',1)
-Test=newDataFrame[T-31-1:T-1].drop('id',1)
+
+TestSize=31
+Train = newDataFrame[0:T-TestSize-2].drop('id',1)
+Test=newDataFrame[T-TestSize-1:T-1].drop('id',1)
 writer = pd.ExcelWriter('D:\\output.xlsx')
 newDataFrame.to_excel(writer,'Sheet1')
 #~~~~~~~~~~~~~~~~~~~~~~~ tensorFlow ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-FEATURES=columnsNames[1:10]
+FEATURES=columnsNames[1:7]
 LABEL="Y"
 
 regressor = lr.DNNRegressor(
     feature_columns=[tf.contrib.layers.real_valued_column(k) for k in FEATURES],
-    hidden_units=[1024, 512, 256]) #1024, 512, 256
+    hidden_units=[1024, 512, 256] , model_dir=r"C:\Users\T\AppData\Local\Temp\tmphju2fe9s\model.ckpt" ) #1024, 512, 256 # model_dir="/tmp/The_model"
 
 # Input builders
 def input_fn(data_set):
@@ -62,7 +65,9 @@ def input_fn(data_set):
   return feature_cols, labels
 
 
-regressor.fit(input_fn=lambda: input_fn(Train), steps=200)
+
+
+regressor.fit(input_fn=lambda: input_fn(Train), steps=400)
 
 
 #ev = regressor.evaluate(input_fn=lambda: input_fn(Test), steps=1)
@@ -70,6 +75,8 @@ regressor.fit(input_fn=lambda: input_fn(Train), steps=200)
 #print("Loss: {0:f}".format(loss_score))
 
 # Print out predictions
+y_real=Test['Y']
+#Test['Y']=0 # to ensure that Y not included in prediction process
 y = regressor.predict(input_fn=lambda: input_fn(Test))
 # .predict() returns an iterator; convert to a list and print predictions
 predictions = list(y)
@@ -77,13 +84,13 @@ print("Predictions: {}".format(str(predictions)))
 
 
 ##### summary #####
-y_real=Test['Y']
+
 result=pd.DataFrame({'y_real':y_real, 'y_predicted':predictions})
 res=result.values
 res[:,0]= (res[:,0] +1)*((MaxLoad-MinLoad)/2) + MinLoad
 res[:,1]= (res[:,1] +1)*((MaxLoad-MinLoad)/2) + MinLoad
 
-result.values
+result
 MAPE= np.mean(abs(res[:,0]-res[:,1])/res[:,0])*100
 print({'MAPE =':MAPE})
 print("--- %s seconds ---" % (time.time() - start_time))
